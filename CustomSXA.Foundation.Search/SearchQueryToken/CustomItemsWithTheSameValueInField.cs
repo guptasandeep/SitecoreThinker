7e32632a5d13821d20fff6da9f739cd0b493f31b
Gupta Sandeepkumar <gupta.sandeepkumar@perficient.com>
2022-08-29 00:37:44 +0530

    Add project files.

---
+++ CustomSXA.Foundation.Search/SearchQueryToken/CustomItemsWithTheSameValueInField.cs
@@ -0,0 +1,53 @@
+﻿using Sitecore.ContentSearch.Utilities;
+using Sitecore.Data.Fields;
+using Sitecore.XA.Foundation.Search.Attributes;
+using Sitecore.XA.Foundation.Search.Pipelines.ResolveSearchQueryTokens;
+using System;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+
+namespace CustomSXA.Foundation.Search.SearchQueryToken
+{
+    public class CustomItemsWithTheSameValueInField : ResolveSearchQueryTokensProcessor
+    {
+        protected string TokenPart { get; } = nameof(CustomItemsWithTheSameValueInField);
+
+        protected string Operation { get; set; } = "should";
+
+        [SxaTokenKey]
+        protected override string TokenKey => FormattableString.Invariant(FormattableStringFactory.Create("{0}|FieldName", (object)this.TokenPart));
+
+        public override void Process(ResolveSearchQueryTokensEventArgs args)
+        {
+            if (args.ContextItem == null)
+                return;
+            for (int index = 0; index < args.Models.Count; ++index)
+            {
+                SearchStringModel model = args.Models[index];
+                if (model.Type.Equals("sxa") && this.ContainsToken(model))
+                {
+                    string str = model.Value.Replace(this.TokenPart, string.Empty).TrimStart('|');
+                    Field field = args.ContextItem.Fields[str];
+                    if (field != null)
+                    {
+                        this.Operation = model.Operation; //setting the operation given in the scope
+                        args.Models.Insert(index, this.BuildModel(str, field.Value)); //pass the field value for filter
+                        args.Models.Remove(model);
+                    }
+                }
+            }
+        }
+
+        protected virtual SearchStringModel BuildModel(
+          string replace,
+          string fieldValue)
+        {
+            return new SearchStringModel("custom", FormattableString.Invariant(FormattableStringFactory.Create("{0}|{1}", (object)replace.ToLowerInvariant(), (object)fieldValue)))
+            {
+                Operation = this.Operation
+            };
+        }
+
+        protected override bool ContainsToken(SearchStringModel m) => Regex.Match(m.Value, FormattableString.Invariant(FormattableStringFactory.Create("{0}\\|[a-zA-Z ]*", (object)this.TokenPart))).Success;
+    }
+}
